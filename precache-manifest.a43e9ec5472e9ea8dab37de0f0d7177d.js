ef1e388cc62492ae693380aec5010b76c996f08a
MHariKrishna8 <mangalampalliharikrishna8@gmail.com>
2020-05-22 01:58:14 +0530

    Updates

---
+++ precache-manifest.a43e9ec5472e9ea8dab37de0f0d7177d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962ccaf3b2c3a00c02ebf7c7abacf563",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b91088c7c9a31609cb97",
+    "url": "/todolist/static/css/2.7d221074.chunk.css"
+  },
+  {
+    "revision": "822617b0d40e71fa0a46",
+    "url": "/todolist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b91088c7c9a31609cb97",
+    "url": "/todolist/static/js/2.95e286a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todolist/static/js/2.95e286a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822617b0d40e71fa0a46",
+    "url": "/todolist/static/js/main.1a1f3179.chunk.js"
+  },
+  {
+    "revision": "b7250e51aa13a32c8099",
+    "url": "/todolist/static/js/runtime-main.c35d7a43.js"
+  }
+]);
